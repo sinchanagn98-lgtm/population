d10eb025a8aa706dcf19c94de8b32e7a0e946fbf
sinchana <sinchanagn98@gmail.com>
2026-06-04 16:34:29 +0530

    Update 1_📊_Population_Analytics.py

--- pages/1_📊_Population_Analytics.py
+++ pages/1_📊_Population_Analytics.py
@@ -1,1 +1,24 @@
+import streamlit as st
+from utils.loader import load_data
+import plotly.express as px
 
+st.title("📊 Population Analytics")
+
+df = load_data()
+
+fig = px.line(
+    df,
+    x="Year",
+    y="Population",
+    markers=True,
+    color="Population"
+)
+
+fig.update_layout(
+    template="plotly_dark"
+)
+
+st.plotly_chart(
+    fig,
+    use_container_width=True
+)
